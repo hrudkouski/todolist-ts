77ca8494d61a4849fac03a141da7f20c5e4d8974
hrudkouski <aprilshower19@gmail.com>
2021-04-28 12:40:39 +0300

    Updates

---
+++ precache-manifest.782cdf690be398257d01ab349015269f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4819d79c68c7f1b63e18e028087ac09e",
+    "url": "/todolist-ts/index.html"
+  },
+  {
+    "revision": "e4853f174a86e4fecd88",
+    "url": "/todolist-ts/static/css/main.8995c464.chunk.css"
+  },
+  {
+    "revision": "8687e3895d0165189ec3",
+    "url": "/todolist-ts/static/js/2.38b57ddc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist-ts/static/js/2.38b57ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4853f174a86e4fecd88",
+    "url": "/todolist-ts/static/js/main.3069d021.chunk.js"
+  },
+  {
+    "revision": "b7c7c0b009241d8831c6",
+    "url": "/todolist-ts/static/js/runtime-main.8e0caec2.js"
+  }
+]);
